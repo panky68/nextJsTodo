c4e0bf5c24c06f5bcd6965f41e510a8b9cc21228
Pankaj Patel <panky_68@hotmail.com>
2024-08-29 15:28:59 +0100

    added icon, created bas layout and added dummy about us page

---
+++ app/about/page.tsx
@@ -0,0 +1,5 @@
+const About = () => {
+  return <div className="text-red-500">Hello</div>;
+};
+
+export default About;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-
-export default function Home() {
+const Home = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Test Page</p>
-        
+      <p>Todos App</p>
     </main>
   );
-}
+};
+
+export default Home;
